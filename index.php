1f4148a77a5632b28ee8aacb64142fa7268c7454
光安雅義 <lightcheap358@gmail.com>
2021-11-03 01:19:42 +0900

    print_r -> echo

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 //アクセストークン
 $accsessToken = 'HlpevqaNfDeMOeHfBKr/5I2adUrmaP7KF5lhtIcV9h9fGKAvEbFfeQw+tvAUBCuX+LZS/DyVakdyG+2jzkl6yqe/4m85WYZoWIL9EVMSi1iS6iotUYdqpsS31bJrkCeyGEA0CyghpsI86M4KDlZj7wdB04t89/1O/w1cDnyilFU=';
 $jsonString = file_get_contents('php://input');
-print_r($jsonString);
+echo($jsonString);
